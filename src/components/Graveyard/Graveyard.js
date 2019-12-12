2453a43d202e976d427ecea14069fd42be242505
Maggie Greene <maggie.greene96@gmail.com>
2019-12-12 14:26:57 -0600

    Set state of passed students

---
+++ src/components/Graveyard/Graveyard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Graveyard extends React.Component {
+  render() {
+    return (
+      <div className="graveyard col-6">
+        <p>Hellooooo GraveYarddd</p>
+      </div>
+    );
+  }
+}
+
+export default Graveyard;
