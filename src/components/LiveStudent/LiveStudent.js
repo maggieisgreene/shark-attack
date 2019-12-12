36e45c1f43a1864a41db48e2bca16e7749a64aa9
Maggie Greene <maggie.greene96@gmail.com>
2019-12-11 20:12:42 -0600

    Work in Progress

--- src/components/LiveStudent/LiveStudent.js
+++ src/components/LiveStudent/LiveStudent.js
@@ -1,2 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import studentShape from '../../helpers/propz/studentShape';
+
+class LiveStudent extends React.Component {
+  static propTypes = {
+    student: studentShape.studentShape,
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <p>Cards will go hereeee!!!!</p>
+      </div>
+    );
+  }
+}
+
+export default LiveStudent;
